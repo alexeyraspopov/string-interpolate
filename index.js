5e2522b1ee284f82f098a38073392c01d796e355
Avenger7x <avenger7x13@gmail.com>
2014-03-16 22:52:17 +0200

    s/key/keypath

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ module.exports = function interpolate(template, data){
 		return interpolate.bind(null, template);
 	}
 
-	return template.replace(/\{([^\}]+)\}/g, function(match, key){
-		var tokens = key.trim().split('.');
+	return template.replace(/\{([^\}]+)\}/g, function(match, keypath){
+		var tokens = keypath.trim().split('.');
 
 		return tokens.reduce(function(data, key){
 			return data[key];
